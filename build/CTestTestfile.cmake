5914a148bfe7dbaf50f199098f37a378e0453de9
Steve Han <ccckblaze@sina.com>
2016-07-05 10:47:53 +0800

    init ros env

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/Documents/ROS Projects/pi_roombot/src
+# Build directory: /home/pi/Documents/ROS Projects/pi_roombot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
