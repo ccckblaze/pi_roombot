adc31dfa20728c820ea21d2e34191ae295c1a5c1
ccckblaze <ccckblaze@sina.com>
2016-07-24 11:47:07 +0800

    add missing files

--- devel/setup.sh
+++ devel/setup.sh
@@ -8,7 +8,7 @@
 
 # since this file is sourced either use the provided _CATKIN_SETUP_DIR
 # or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/pi/Documents/ROS Projects/pi_roombot/devel}
+: ${_CATKIN_SETUP_DIR:=/home/steven/Documents/ROS Projects/test_ws/devel}
 _SETUP_UTIL="$_CATKIN_SETUP_DIR/_setup_util.py"
 unset _CATKIN_SETUP_DIR
 
