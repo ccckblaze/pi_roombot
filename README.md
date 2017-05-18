d920052c721959a661e0fbe0baffd6c80fd3d2a8
ccckblaze <ccckblaze@sina.com>
2017-05-18 14:25:37 +0800

    change repo struction

---
+++ README.md
@@ -0,0 +1,2 @@
+trouble shooting:
+1. rosserial lose sync: #define USE_USBCON before include ros.h 
--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/pi/Documents/ROS Projects/pi_roombot/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/opt/ros/indigo".split(';') if "/opt/ros/indigo" != "" else []
